b52340373e9811fd92dfcda8c0e6b4ab0b651b84
estebangomez <gomez.esteban@outlook.fr>
2024-12-21 16:10:09 +0100

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+import EditMap from "~/components/EditMap.vue";
+</script>
+
+<template>
+  <header>
+    <h1 class="parkinsans-title">
+      Add pins to your <span>maps</span>
+    </h1>
+  </header>
+  <EditMap />
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: center;
+}
+
+h1 span {
+  text-decoration: underline;
+}
+</style>
